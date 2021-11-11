6e334149959fe86e5ee862f9ebb54f9c33fba0a8
alireza <alireza.zabihi@yahoo.com>
2021-11-11 06:49:07 +0330

    first version of it

---
+++ main.js
@@ -0,0 +1,36 @@
+function grid(size) {
+    let width = 600 / size;
+    const container = document.querySelector(".container");
+    let div;
+    for (let i = 0; i < size * size; i++) {        
+        div = document.createElement('div');
+        div.style.cssText = `width: ${width}px; height: ${width}px; margin: 0;`;
+        div.classList.add("grid-child")
+        container.appendChild(div);
+    }
+    const gridChilds = document.querySelectorAll(".grid-child")
+    gridChilds.forEach((element) => element.addEventListener('mouseenter', changeColor))
+    function changeColor(event) {
+        this.classList.add('grid-child-hover')
+    }
+}
+
+function main() {
+    let size = 16;
+    grid(size)
+    const container = document.querySelector(".container");
+    const resetButton = document.querySelector('#reset')
+    resetButton.onclick = reset
+    let gridChilds;
+    function reset() {
+        gridChilds = document.querySelectorAll(".grid-child")
+        gridChilds.forEach((element) => element.classList.remove("grid-child-hover"))
+        do {
+        size = prompt("what size should the new grid be?")
+        } while (size > 100 || size < 5);
+        gridChilds.forEach((element) => element.remove())
+        grid(size)
+    }
+}
+
+main()
