39e2d608cba81c875df6234ea0f2db887044c0b6
alireza <alireza.zabihi@yahoo.com>
2022-06-26 10:27:58 +0430

    all functions done

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-/*
-function grid(size) {
-    const containerWidth = 600;
-    let width = Math.floor(containerWidth / size);
-    const container = document.querySelector(".container");
-    container.style.width = `${width * size}px`
-    let div;
-    for (let i = 0; i < size * size; i++) {        
-        div = document.createElement('div');
-        div.style.cssText = `width: ${width}px; height: ${width}px; margin: 0;`;
-        div.classList.add("grid-child")
-        container.appendChild(div);
-    }
-    const gridChilds = document.querySelectorAll(".grid-child")
-    gridChilds.forEach((element) => element.addEventListener('mouseenter', changeColor))
-    function changeColor(event) {
-        this.classList.add('grid-child-hover')
-    }
-}
-
-function main() {
-    let size = 16;
-    grid(size)
-    const container = document.querySelector(".container");
-    const resetButton = document.querySelector('#reset')
-    resetButton.onclick = reset
-    let gridChilds;
-    function reset() {
-        gridChilds = document.querySelectorAll(".grid-child")
-        gridChilds.forEach((element) => element.classList.remove("grid-child-hover"))
-        do {
-        size = prompt("what size should the new grid be?")
-        } while (size > 100 || size < 5);
-        gridChilds.forEach((element) => element.remove())
-        grid(size)
-    }
-}
-
-main()
-*/
 let backgroundColor = "rgb(204, 229, 230)";
 let color = "rgb(8, 8, 8)";
 let mode = 'color';
@@ -48,6 +8,15 @@ const rainbowMode = document.querySelector('.rainbow-mode');
 const colorMode = document.querySelector('.color-mode')
 const eraserMode = document.querySelector('.eraser-mode')
 const modes = document.querySelectorAll('.mode')
+const clear = document.querySelector('.clear')
+const setBackground = document.querySelector('.background-color-picker')
+const size = document.querySelector('.size')
+
+let gridItems;
+
+// reseting the color pickers
+colorPicker.value = color
+setBackground.value = backgroundColor
 
 // default grid
 createGrid(grid, 20);
@@ -68,6 +37,7 @@ function createGrid(grid, size) {
             gridrow.appendChild(girdElement);
         }
     })
+    addEventToGridItems()
 }
 
 // check for mousebutton being down
@@ -76,13 +46,15 @@ let mouseDown;
 document.body.addEventListener('mousedown', () => mouseDown = true, {capture: true});
 document.body.addEventListener('mouseup', () => mouseDown = false);
 
-//
-let gridItems = document.querySelectorAll('.grid-item');
+// add eventlistener to grid items
+function addEventToGridItems() {
+    gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => {
+        item.addEventListener('mouseover', changeColor);
+        item.addEventListener('mousedown', changeColor);
+    })
+}
 
-gridItems.forEach(item => {
-    item.addEventListener('mouseover', changeColor);
-    item.addEventListener('mousedown', changeColor);
-})
 
 function changeColor(e) {
     // doesn't do anything if mouse isnot down
@@ -92,7 +64,7 @@ function changeColor(e) {
     } else if (mode === 'rainbow') {
         this.style.backgroundColor = getRandomColor();
     } else if (mode === 'eraser') {
-        this.style.backgroundColor = backgroundColor;
+        this.style.backgroundColor = '';
     }
 }
 
@@ -101,7 +73,7 @@ function getRandomColor() {
     let red = Math.floor(Math.random()*255);
     let green = Math.floor(Math.random()*255);
     let blue = Math.floor(Math.random()*255);
-    return `rgb(${red}, ${green}, ${blue})`
+    return `rgb(${red}, ${green}, ${blue})`;
 }
 
 // change the color with color picker
@@ -123,7 +95,7 @@ rainbowMode.addEventListener('click', e => {
     e.target.classList.add('selected');
 })
 
-// change the mode to rainbow
+// change the mode to eraser
 eraserMode.addEventListener('click', e => {
     mode = 'eraser';
     clearSelection()
@@ -132,8 +104,34 @@ eraserMode.addEventListener('click', e => {
 
 function clearSelection() {
     modes.forEach(mode => {
-        mode.classList.remove('selected')
+        mode.classList.remove('selected');
     })
 }
 
-//continue with clear button
+// clear button
+clear.addEventListener('click', e => {
+    gridItems.forEach(item => {
+        item.style.backgroundColor = '';
+    })
+})
+
+// change background button
+setBackground.addEventListener('change', e => {
+    backgroundColor = e.target.value;
+    grid.style.backgroundColor = backgroundColor
+})
+
+// create new grid with range slider
+size.addEventListener('change', e => {
+    grid.innerHTML = ''
+    createGrid(grid, e.target.value)
+})
+
+
+
+
+
+
+
+
+
